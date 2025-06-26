3d4bf8d5087720a78d7cafe42401a6eec43d4bc7
DerErfinder123 <samuelroessel@web.de>
2025-06-26 17:51:48 +0200

    1
    1

--- web multi game sever/server.py
+++ web multi game sever/server.py
@@ -1,23 +1,33 @@
-import socket,threading
+import socket
+import threading
+
 server_ip = "0.0.0.0"
 server_port = 8000
-anzahl_threads = 0
-server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server.bind(server_ip,server_port)
-server.listen(2)
-client_name = dict()
-while anzahl_threads < 2:
-    client_socket, client_adress = server.accept()
 
+server_adress = (server_ip, server_port)
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(server_adress)
+server.listen(2)  # Maximal 2 Verbindungen
+
+client_name = dict()
 
 def handle_client(client_socket, client_address):
-    client_socket.send("Welcome to the server!gebe namen ein".encode("utf-8"))
-    request = client_socket.recv(1024)
-    request = request.decode("utf-8")
-    client_name[client_address] = request
+    client_socket.send("Willkommen auf dem Server! Bitte gib deinen Namen ein: ".encode("utf-8"))
+    name = client_socket.recv(1024).decode("utf-8")
+    client_name[client_address] = name
+    print(f"Client {client_address} verbunden als {name}")
     while True:
         request = client_socket.recv(1024)
-        client_socket.send(client_name[client_address], request.encode("utf-8"))
+        client_socket.send()
+
+print("Server läuft...")
+
+while True:
+    client_socket, client_address = server.accept()
+    print("Client verbunden:", client_address)
+    thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
+    thread.start()
+
 
 
 
