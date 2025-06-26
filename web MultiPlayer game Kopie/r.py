047a0ba5f9e036c37ddce3e934111b4687d5ef77
DerErfinder123 <samuelroessel@web.de>
2025-06-26 16:26:20 +0200

    1

--- web MultiPlayer game Kopie/r.py
+++
@@ -1,7 +0,0 @@
-        map_width = self.tile_map.width * self.tile_map.tile_width
-        map_height = self.tile_map.height * self.tile_map.tile_height
-
-        # Kamera folgt dem Spieler, bleibt aber im Fenster
-        cam_x = max(self.width // 2, min(self.spieler.center_x, map_width - self.width // 2))
-        cam_y = max(self.height // 2, min(self.spieler.center_y, map_height - self.height // 2))
-        self.camera.position = (cam_x, cam_y)
