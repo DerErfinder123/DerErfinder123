cca5e9ede00a64417e0e53dcf2a31063c6f87529
DerErfinder123 <samuelroessel@web.de>
2025-11-25 17:23:47 +0100

    Add mob spawning and cleanup logic
    Introduces a MobTimer node and connects its timeout signal to spawn mobs at random locations along a closed path. Mobs are now freed when they exit the screen, and the main scene and mob scripts have been updated to support this behavior. Also includes minor adjustments to player collision and scene layout.

--- 3d_squash_the_creeps_starter/main.gd
+++ 3d_squash_the_creeps_starter/main.gd
@@ -1,2 +1,13 @@
 extends Node3D
-@export var mob_sence: PackedScene
+@export var mob_scence: ""
+
+
+func _on_mob_timer_timeout() -> void:
+	var 	mob = mob_scence.initialize()# Replace with function body.
+	
+	var mob_spawn_location = $Path3D/SpawnLocation
+	mob_spawn_location.progress_ratio = randf()
+	var player_position = $player.position
+	mob.initialize(mob_spawn_location.position, player_position)
+	
+	add_child(mob)
--- 3d_squash_the_creeps_starter/mob.gd
+++ 3d_squash_the_creeps_starter/mob.gd
@@ -12,5 +12,8 @@ func initialize(start_position, player_position):
 
 func _physics_process(delta: float) -> void:
 	move_and_slide()
-func _on_visible_on_screen_notifier_3d_screen_excited():
-	pass
+
+
+
+func _on_visible_on_screen_notifier_3d_screen_exited() -> void:
+	queue_free()# Replace with function body.
