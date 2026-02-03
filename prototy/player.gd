9d1aff585a0769c7188180eec594cbb87f117256
DerErfinder123 <samuelroessel@web.de>
2026-02-03 17:31:21 +0100

    t
    z

---
+++ prototy/player.gd
@@ -0,0 +1,36 @@
+extends CharacterBody3D
+@export var speed = 14
+@export var fall_acceleration = 75
+@export var jump_impuls = 20
+@export var bounce_impuls = 16
+
+var target_velocity = Vector3.ZERO
+
+func _physics_process(delta: float) -> void:
+	var direction = Vector3.ZERO
+	
+	if Input.is_action_pressed("move_right"):
+		direction.x += 1
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("move_backwards"):
+		direction.z += 1
+	if Input.is_action_pressed("move_forward"):
+		direction.z -= 1
+		
+	if direction != Vector3.ZERO:
+		direction = direction.normalized()
+		
+	$".".basis = Basis.looking_at(direction)
+	target_velocity.x = direction.x * speed
+	target_velocity.z = direction.z * speed
+	
+	if not is_on_floor():
+		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		target_velocity.y = jump_impuls
+		
+	
+	
+	velocity = target_velocity
+	move_and_slide()
